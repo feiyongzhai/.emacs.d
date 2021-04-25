25325acc7f623f1d1e73e743cad0ddeaea8cd43d
yongfei-mint <yongfeizhai@outlook.com>
2021-04-25 23:50:14 +0800

    避免刚启动时无法使用dired-jump[-other-window]
    准确来说应该是相应的快捷键 C-x [4] C-j

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -12,8 +12,18 @@
 
 ;; 这个快捷键绑定似乎已经写在了 diredx.el 文件中，使用 `dired-jump' 之
 ;; 后命令会被重新绑定快捷键
-(global-set-key (kbd "C-x C-j") 'dired-jump)
-
+(global-set-key (kbd "C-x C-j") (lambda ()
+				  (interactive)
+				  (if (commandp 'dired-jump)
+				      (dired-jump)
+				    (require 'dired-x)
+				    (dired-jump))))
+(global-set-key (kbd "C-x 4 C-j") (lambda ()
+				    (interactive)
+				    (if (commandp 'dired-jump-other-window)
+					(dired-jump-other-window)
+				      (require 'dired-x)
+				      (dired-jump-other-window))))
 ;;; 自动打开高亮行
 (add-hook 'dired-mode-hook (lambda ()
 			     (hl-line-mode 1)

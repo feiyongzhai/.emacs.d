1f64955a5f6cf23d861612a062b7bdc2086a569f
yongfei-mint <yongfeizhai@outlook.com>
2021-04-25 15:13:22 +0800

    add shortkeys for company-mode
    更新了README.md文件

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -8,11 +8,14 @@
 ;;; Keys tweaks
 (with-eval-after-load 'company
 
+  (define-key company-mode-map (kbd "M-/") #'company-complete)
+  (define-key company-mode-map (kbd "C-M-i") #'company-complete)
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil)
   (define-key company-active-map (kbd "C-n") #'company-select-next)
   (define-key company-active-map (kbd "C-p") #'company-select-previous)
-
+  (define-key company-active-map (kbd "M-/") #'company-other-backend)
+    
   (setq company-idle-delay .1
       company-minimum-prefix-length 1)
   )

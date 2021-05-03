25261bada7d37fb4861d6986d1409fa1edea38c8
yongfei-mint <yongfeizhai@outlook.com>
2021-05-03 21:01:44 +0800

    improvement(ido): ido-use-virtual-buffers toggle on

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -8,6 +8,7 @@
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
 (setq ido-max-window-height 1)
+(setq ido-use-virtual-buffers t)
 (setq smex-prompt-string "^_^ ")
 (global-set-key (kbd "M-x") 'smex)
 

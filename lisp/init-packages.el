9e137938793d8991e183401d67167f1a389c2c61
yongfei-win10 <yongfeizhai@outlook.com>
2021-05-08 10:56:27 +0800

    idea(packages): 想添加一个新的关于`youdao-dictionary'的函数

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -14,6 +14,13 @@
 
 (global-set-key (kbd "C-c y y") #'youdao-dictionary-search-at-point-tooltip)
 (global-set-key (kbd "C-c y i") #'youdao-dictionary-search-from-input)
+(defun user/youdao-dictionary-search-from-input (_arg)
+  (interactive "P")
+  (message "施工中")
+  (when _arg
+    ;; TODO: 调用`youdao-dictionary-search-from-input'后自动启用minibuffer的输入法
+    (message "施工中")
+    ))
 
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
 

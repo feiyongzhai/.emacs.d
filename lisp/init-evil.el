856fb0a331e4e750689260a8adb967a5631805ec
yongfei-win10 <yongfeizhai@outlook.com>
2021-06-05 19:30:09 +0800

    improve(evil): autoload `evil-local-mode'

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -5,5 +5,7 @@
 ;;; Add to list `fei-package-ensure-installed'
 (add-to-list 'fei-package-ensure-installed 'evil)
 
+(autoload 'evil-local-mode "evil" nil t)
+
 (provide 'init-evil)
 ;;; init-evil.el ends here

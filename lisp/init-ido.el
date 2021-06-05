6ee87b53889dc0fa764dbb660fd9ab5f5ab0a2d6
yongfei-win10 <yongfeizhai@outlook.com>
2021-06-05 14:03:00 +0800

    improve(ido): disable `auto-merge-work-directories'

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -9,6 +9,9 @@
 (setq ido-enable-flex-matching t)
 (setq ido-max-window-height 1)
 (setq ido-use-virtual-buffers t)
+;; Disable auto merge work directories behavior, But you can merge
+;; manually by M-s undo merge by C-z manually
+(setq ido-auto-merge-work-directories-length -1)
 (setq smex-prompt-string "^_^ ")
 (global-set-key (kbd "M-x") 'smex)
 

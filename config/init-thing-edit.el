c2d515de9cdbeed4fb0a2d254fea21d00e565c7f
yongfei-mint <yongfeizhai@outlook.com>
2021-06-11 12:49:10 +0800

    refactor(thing-edit): use fei-define-key-with-map

--- config/init-thing-edit.el
+++ config/init-thing-edit.el
@@ -11,47 +11,49 @@
 
 ;;; Code:
 
-(progn
-  (require 'thing-edit)
-  (defvar user-thing-edit-map (make-sparse-keymap)
-    "Keybinding for `thing-edit'")
 
-  ;; Copy.
-  (define-key user-thing-edit-map (kbd "w") 'thing-copy-word)
-  (define-key user-thing-edit-map (kbd "s") 'thing-copy-symbol)
-  (define-key user-thing-edit-map (kbd "m") 'thing-copy-email)
-  (define-key user-thing-edit-map (kbd "f") 'thing-copy-filename)
-  (define-key user-thing-edit-map (kbd "u") 'thing-copy-url)
-  (define-key user-thing-edit-map (kbd "x") 'thing-copy-sexp)
-  (define-key user-thing-edit-map (kbd "t") 'thing-copy-page)
-  (define-key user-thing-edit-map (kbd "g") 'thing-copy-sentence)
-  (define-key user-thing-edit-map (kbd "o") 'thing-copy-whitespace)
-  (define-key user-thing-edit-map (kbd "i") 'thing-copy-list)
-  (define-key user-thing-edit-map (kbd "c") 'thing-copy-comment)
-  (define-key user-thing-edit-map (kbd "h") 'thing-copy-defun)
-  (define-key user-thing-edit-map (kbd "p") 'thing-copy-parentheses)
-  (define-key user-thing-edit-map (kbd "a") 'thing-copy-to-line-beginning)
-  (define-key user-thing-edit-map (kbd "e") 'thing-copy-to-line-end)
-  (define-key user-thing-edit-map (kbd "l") 'thing-copy-line)
+(require 'thing-edit)
+(defvar user-thing-edit-map (make-sparse-keymap)
+  "Keybinding for `thing-edit'")
 
+;; Copy.
+(fei-define-key-with-map user-thing-edit-map
+  '(("w" . thing-copy-word)
+    ("s" . thing-copy-symbol)
+    ("m" . thing-copy-email)
+    ("f" . thing-copy-filename)
+    ("u" . thing-copy-url)
+    ("x" . thing-copy-sexp)
+    ("t" . thing-copy-page)
+    ("g" . thing-copy-sentence)
+    ("o" . thing-copy-whitespace)
+    ("i" . thing-copy-list)
+    ("c" . thing-copy-comment)
+    ("h" . thing-copy-defun)
+    ("p" . thing-copy-parentheses)
+    ("a" . thing-copy-to-line-beginning)
+    ("e" . thing-copy-to-line-end)
+    ("l" . thing-copy-line)
+    ))
 
-  ;; Replace
-  (define-key user-thing-edit-map (kbd "W") 'thing-replace-word)
-  (define-key user-thing-edit-map (kbd "S") 'thing-replace-symbol)
-  (define-key user-thing-edit-map (kbd "M") 'thing-replace-email)
-  (define-key user-thing-edit-map (kbd "F") 'thing-replace-filename)
-  (define-key user-thing-edit-map (kbd "U") 'thing-replace-url)
-  (define-key user-thing-edit-map (kbd "X") 'thing-replace-sexp)
-  (define-key user-thing-edit-map (kbd "T") 'thing-replace-page)
-  (define-key user-thing-edit-map (kbd "G") 'thing-replace-sentence)
-  (define-key user-thing-edit-map (kbd "O") 'thing-replace-whitespace)
-  (define-key user-thing-edit-map (kbd "I") 'thing-replace-list)
-  (define-key user-thing-edit-map (kbd "C") 'thing-replace-comment)
-  (define-key user-thing-edit-map (kbd "H") 'thing-replace-defun)
-  (define-key user-thing-edit-map (kbd "P") 'thing-replace-parentheses)
-  (define-key user-thing-edit-map (kbd "L") 'thing-replace-line)
-
-  (global-set-key (kbd "C-z") user-thing-edit-map))
+;; Replace
+(fei-define-key-with-map user-thing-edit-map
+  '(("W" . thing-replace-word)
+    ("S" . thing-replace-symbol)
+    ("M" . thing-replace-email)
+    ("F" . thing-replace-filename)
+    ("U" . thing-replace-url)
+    ("X" . thing-replace-sexp)
+    ("T" . thing-replace-page)
+    ("G" . thing-replace-sentence)
+    ("O" . thing-replace-whitespace)
+    ("I" . thing-replace-list)
+    ("C" . thing-replace-comment)
+    ("H" . thing-replace-defun)
+    ("P" . thing-replace-parentheses)
+    ("L" . thing-replace-line)
+    ))
+(global-set-key (kbd "C-z") user-thing-edit-map)
 
 (provide 'init-thing-edit)
 ;;; init-thing-edit.el ends here

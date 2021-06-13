1f09c460900ab7f33ad4cd1f7556c5676727c615
yongfei-mint <yongfeizhai@outlook.com>
2021-06-14 03:16:48 +0800

    feat(evil): `evil-local-mode-with-cursor' & key

--- config/init-evil.el
+++ config/init-evil.el
@@ -2,9 +2,24 @@
 
 ;;; Code:
 
-(global-set-key (kbd "ESC ESC <M-escape>") #'evil-local-mode)
+(global-set-key (kbd "ESC ESC <M-escape>") #'evil-local-mode-with-cursor)
 
 (autoload 'evil-local-mode "evil" nil t)
+
+;;; Vars
+
+(setq evil-emacs-state-cursor 'bar)
+
+;;; Func
+
+(defun evil-local-mode-with-cursor ()
+  (interactive)
+  (unless (boundp 'evil-local-mode)
+    (evil-local-mode -1)) ;; unless part is for initialization
+  (if evil-local-mode
+      (progn (evil-local-mode -1)
+	     (setq cursor-type 'bar))
+    (evil-local-mode 1)))
 
 (provide 'init-evil)
 ;;; init-evil.el ends here

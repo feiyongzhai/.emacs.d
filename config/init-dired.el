1b96bc076da9c2642ea1b877cc0259b723f8a52b
yongfei-mint <yongfeizhai@outlook.com>
2021-06-13 17:30:14 +0800

    feat(dired): customize func -> `dired-toggle-hidden'

--- config/init-dired.el
+++ config/init-dired.el
@@ -14,12 +14,29 @@
   (define-key dired-mode-map (kbd "W") 'dired-copy-filename-as-kill)
 
   (define-key dired-mode-map (kbd "b") 'dired-up-directory)
-  (define-key dired-mode-map (kbd "C-d") #'bookmark-set))
+  (define-key dired-mode-map (kbd "C-d") #'bookmark-set)
+
+  (define-key dired-mode-map (kbd "M-h") #'dired-toggle-hidden))
 
 ;;; Vars
 
 (add-hook 'dired-mode-hook #'hl-line-mode)
 (add-hook 'dired-mode-hook #'dired-hide-details-mode)
+
+(setq dired-listing-switches "-Bhl --group-directories-first")
+
+;;; Func
+
+(defvar dired-toggle-hidden nil
+  "t means on, nil means off")
+
+(defun dired-toggle-hidden ()
+    (interactive)
+    (if dired-toggle-hidden
+	(progn (dired-sort-other "-Bhl  --group-directories-first")
+	       (setq dired-toggle-hidden nil))
+      (dired-sort-other (concat dired-listing-switches " -a"))
+      (setq dired-toggle-hidden t)))
 
 
 (provide 'init-dired)

fa73354afac373b905878b45dd07382fab32e050
yongfei-mint <yongfeizhai@outlook.com>
2021-06-23 00:21:59 +0800

    improve(elfeed): Add some feeds

--- config/init-elfeed.el
+++ config/init-elfeed.el
@@ -4,7 +4,14 @@
         ("http://www.solidot.org/index.rss" Solidot)
 	("https://manateelazycat.github.io/feed.xml" ManateeLazyCat)
 	("https://sspai.com/feed" sspai)
-        ))
+        ("https://planet.emacslife.com/atom.xml" emacs community)
+        ("http://feed.pippa.io/public/shows/teamhuman" podcast culture)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UC0uTPqBCFIpZxlz_Lv1tk_g" personal video)
+	("https://emacs-china.org/latest.rss" emacs)
+	("https://www.zhihu.com/rss" 知乎)
+	("http://songshuhui.net/feed" 松鼠科学会)
+	("http://www.geekpark.net/rss" 极客公园)
+	))
 
 
 (provide 'init-elfeed)

020cf49b802a719478cdecce2fb3880aa361e0ec
yongfei-mint <yongfeizhai@outlook.com>
2021-06-23 10:01:16 +0800

    improve(elfeed): new shortkeys and remove some feeds

--- config/init-elfeed.el
+++ config/init-elfeed.el
@@ -1,14 +1,20 @@
+;;; Keys
+
+(with-eval-after-load 'elfeed
+  (define-key elfeed-search-mode-map (kbd "f") 'elfeed-search-show-entry))
+
+;;; Vars
 
 (setq elfeed-feeds
       '(("https://sachachua.com/blog/feed/" SachaChua)
         ("http://www.solidot.org/index.rss" Solidot)
 	("https://manateelazycat.github.io/feed.xml" ManateeLazyCat)
-	("https://sspai.com/feed" sspai)
+	;; ("https://sspai.com/feed" sspai)
         ("https://planet.emacslife.com/atom.xml" emacs community)
-        ("http://feed.pippa.io/public/shows/teamhuman" podcast culture)
+        ;; ("http://feed.pippa.io/public/shows/teamhuman" podcast culture)
         ("https://www.youtube.com/feeds/videos.xml?channel_id=UC0uTPqBCFIpZxlz_Lv1tk_g" personal video)
 	("https://emacs-china.org/latest.rss" emacs)
-	("https://www.zhihu.com/rss" 知乎)
+	;; ("https://www.zhihu.com/rss" 知乎)
 	("http://songshuhui.net/feed" 松鼠科学会)
 	("http://www.geekpark.net/rss" 极客公园)
 	))

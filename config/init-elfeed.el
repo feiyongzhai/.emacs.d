58a791bcf1d34f70e5e36dc6e646b76ac4d4a43e
yongfei-mint <yongfeizhai@outlook.com>
2021-06-23 22:46:49 +0800

    improve(elfeed): remove four feeds

--- config/init-elfeed.el
+++ config/init-elfeed.el
@@ -6,18 +6,18 @@
 ;;; Vars
 
 (setq elfeed-feeds
-      '(("https://sachachua.com/blog/feed/" SachaChua)
-        ("http://www.solidot.org/index.rss" Solidot)
+      '(("http://www.solidot.org/index.rss" Solidot)
+	("https://sachachua.com/blog/feed/" SachaChua)
 	("https://manateelazycat.github.io/feed.xml" ManateeLazyCat)
-	;; ("https://sspai.com/feed" sspai)
-        ("https://planet.emacslife.com/atom.xml" emacs community)
-        ;; ("http://feed.pippa.io/public/shows/teamhuman" podcast culture)
         ("https://www.youtube.com/feeds/videos.xml?channel_id=UC0uTPqBCFIpZxlz_Lv1tk_g" personal video)
-	("https://emacs-china.org/latest.rss" emacs)
+        ;; ("https://planet.emacslife.com/atom.xml" emacs community)
+	;; ("https://emacs-china.org/latest.rss" emacs)
+        ;; ("http://feed.pippa.io/public/shows/teamhuman" podcast culture)
+	;; ("http://www.geekpark.net/rss" 极客公园)
 	;; ("https://www.zhihu.com/rss" 知乎)
-	("http://songshuhui.net/feed" 松鼠科学会)
-	("http://www.geekpark.net/rss" 极客公园)
+	;; ("https://sspai.com/feed" sspai)
 	))
 
+(setq shr-inhibit-images t)
 
 (provide 'init-elfeed)

bb371bba7eda3b7e3e692c264403402f6d3ff891
yongfei-mint <yongfeizhai@outlook.com>
2021-06-29 18:50:06 +0800

    improve(evil): `fei-show-evil-state` eval after load `evil`

--- config/init-evil.el
+++ config/init-evil.el
@@ -10,27 +10,28 @@
 
 (add-hook 'find-file-hook #'evil-local-mode)
 (add-hook 'help-mode-hook #'evil-local-mode)
-(add-hook 'post-command-hook #'fei-show-evil-state)
 (setq evil-emacs-state-cursor 'bar)
 
 ;;; Funcs
 
 (defvar fei-default-color (cons (face-background 'mode-line)
 				  (face-foreground 'mode-line)))
-(defun fei-show-evil-state ()
-  "Change mode line color to notify user evil current state."
-  (let* ((color (cond ((minibufferp) fei-default-color)
-		      ((evil-insert-state-p) '("#e80000" . "#ffffff"))
-		      ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
-		      ((or (eq this-command 'load-theme)
-			   (eq this-command 'counsel-load-theme)
-			   (eq this-command 'modus-themes-toggle))
-		       (setq fei-default-color
-			     (cons (face-background 'mode-line)
-				   (face-foreground 'mode-line))))
-		      (t fei-default-color))))
-    (set-face-background 'mode-line (car color))
-    (set-face-foreground 'mode-line (cdr color))))
+(with-eval-after-load 'evil
+  (add-hook 'post-command-hook #'fei-show-evil-state)
+  (defun fei-show-evil-state ()
+    "Change mode line color to notify user evil current state."
+    (let* ((color (cond ((minibufferp) fei-default-color)
+			((evil-insert-state-p) '("#e80000" . "#ffffff"))
+			((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+			((or (eq this-command 'load-theme)
+			     (eq this-command 'counsel-load-theme)
+			     (eq this-command 'modus-themes-toggle))
+			 (setq fei-default-color
+			       (cons (face-background 'mode-line)
+				     (face-foreground 'mode-line))))
+			(t fei-default-color))))
+      (set-face-background 'mode-line (car color))
+      (set-face-foreground 'mode-line (cdr color)))))
 
 (defun evil-local-mode-with-cursor ()
   (interactive)

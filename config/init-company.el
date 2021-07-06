957d8a4890fa74280fec7343c47d8ca014504b7e
fei-manjaro-lab <yongfeizhai@qq.com>
2021-07-06 16:00:20 +0800

    feat(company): add copied code from centuar emacs

--- config/init-company.el
+++ config/init-company.el
@@ -34,5 +34,80 @@
     ))
 
 
+;;; {{ company related config copied from centuar emacs
+
+(defconst emacs/>=26p
+  (>= emacs-major-version 26)
+  "Emacs is 26 or above.")
+
+(use-package company
+  :diminish
+  :defines (company-dabbrev-ignore-case company-dabbrev-downcase)
+  :commands company-cancel
+  :bind (:map company-active-map
+	      ("<tab>" . company-complete-common-or-cycle)
+	      ("<backtab>" . my-company-yasnippet)
+	      )
+  :init
+  (setq company-tooltip-align-annotations t
+        company-tooltip-limit 12
+        company-echo-delay (if (display-graphic-p) nil 0)
+        company-require-match nil
+        company-dabbrev-ignore-case nil
+        company-dabbrev-downcase nil
+        company-global-modes '(not erc-mode message-mode help-mode
+                                   gud-mode eshell-mode shell-mode)
+        company-backends '((company-capf :with company-yasnippet)
+                           (company-dabbrev-code company-keywords company-files)
+                           company-dabbrev company-yasnippet))
+
+  (defun my-company-yasnippet ()
+    "Hide the current completeions and show snippets."
+    (interactive)
+    (company-cancel)
+    (call-interactively 'company-yasnippet))
+
+  :config
+  ;; `yasnippet' integration
+  (with-no-warnings
+    (with-eval-after-load 'yasnippet
+      (defun company-backend-with-yas (backend)
+        "Add `yasnippet' to company backend."
+        (if (and (listp backend) (member 'company-yasnippet backend))
+            backend
+          (append (if (consp backend) backend (list backend))
+                  '(:with company-yasnippet))))
+
+      (defun my-company-enbale-yas (&rest _)
+        "Enable `yasnippet' in `company'."
+        (setq company-backends (mapcar #'company-backend-with-yas company-backends)))
+
+      (defun my-lsp-fix-company-capf ()
+        "Remove redundant `comapny-capf'."
+        (setq company-backends
+              (remove 'company-backends (remq 'company-capf company-backends))))
+      (advice-add #'lsp-completion--enable :after #'my-lsp-fix-company-capf)
+
+      (defun my-company-yasnippet-disable-inline (fn cmd &optional arg &rest _ignore)
+        "Enable yasnippet but disable it inline."
+        (if (eq cmd  'prefix)
+            (when-let ((prefix (funcall fn 'prefix)))
+              (unless (memq (char-before (- (point) (length prefix)))
+                            '(?. ?< ?> ?\( ?\) ?\[ ?{ ?} ?\" ?' ?`))
+                prefix))
+          (progn
+            (when (and (bound-and-true-p lsp-mode)
+                       arg (not (get-text-property 0 'yas-annotation-patch arg)))
+              (let* ((name (get-text-property 0 'yas-annotation arg))
+                     (snip (format "%s (Snippet)" name))
+                     (len (length arg)))
+                (put-text-property 0 len 'yas-annotation snip arg)
+                (put-text-property 0 len 'yas-annotation-patch t arg)))
+            (funcall fn cmd  arg))))
+      (advice-add #'company-yasnippet :around #'my-company-yasnippet-disable-inline)))
+  )
+;;; }}
+
+
 (provide 'init-company)
 ;;; init-company.el ends here.

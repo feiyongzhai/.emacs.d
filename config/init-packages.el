fe954abc7bbd2ceca8e5ed037f8d1c70703fbaf4
fei-manjaro-lab <yongfeizhai@qq.com>
2021-07-12 16:53:04 +0800

    fixup `yasnippet-snippets--fixed-indent` void

--- config/init-packages.el
+++ config/init-packages.el
@@ -2,6 +2,14 @@
 
 ;;; Keys
 
+(defun yasnippet-snippets--fixed-indent ()
+  "Set `yas-indent-line' to `fixed'."
+  (set (make-local-variable 'yas-indent-line) 'fixed))
+
+(defun yasnippet-snippets--no-indent ()
+  "Set `yas-indent-line' to nil."
+  (set (make-local-variable 'yas-indent-line) nil))
+
 (with-eval-after-load 'yasnippet
   (yas-load-directory (expand-file-name "~/.emacs.d/snippets") t))
 

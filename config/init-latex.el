c74b6d6f2a9d749b95f6dce976acf4360b52c305
yongfei-mint <yongfeizhai@outlook.com>
2021-07-26 11:39:16 +0800

    feat(latex): add xelatex command support for `C-c C-c`

--- config/init-latex.el
+++ config/init-latex.el
@@ -2,6 +2,11 @@
 
 (add-hook 'LaTeX-mode-hook #'cdlatex-mode)
 
+(with-eval-after-load 'latex
+  ;; @ref https://github.com/yangsheng6810/dotfiles/blob/master/.spacemacs.d/init.org
+  (add-to-list 'TeX-command-list '
+	       ("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t)))
+
 ;; (setq TeX-view-program-selection
 ;;       '(((output-dvi has-no-display-manager)
 ;; 	 "dvi2tty")

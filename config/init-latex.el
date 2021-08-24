74a06fc94132ff937e491ee40133dcbac5abf8f9
fei-manjaro-lab <yongfeizhai@qq.com>
2021-08-24 14:29:22 +0800

    Do not use eaf as pdf viewer in linux platform

--- config/init-latex.el
+++ config/init-latex.el
@@ -7,38 +7,6 @@
   (add-to-list 'TeX-command-list '
 	       ("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t)))
 
-(unless *is-windows*
-;;; eaf support
-  ;; @ref https://github.com/manateelazycat/emacs-application-framework/wiki/LaTeX
-  (defun TeX-eaf-sync-view()
-    (unless (fboundp 'eaf-open)
-      ;; (error "EAF is not available!")
-      (require 'eaf)
-      )
-    (let* ((pdf (TeX-active-master (TeX-output-extension)))
-	   (url (expand-file-name pdf))
-	   (app-name "pdf-viewer")
-	   (exists-eaf-buffer)
-	   (eaf-buffer-window))
-      (catch 'found-match-buffer
-	(dolist (buffer (buffer-list))
-	  (set-buffer buffer)
-	  (when (equal major-mode 'eaf-mode)
-	    (when (and (string= eaf--buffer-url url)
-		       (string= eaf--buffer-app-name app-name))
-	      (setq exists-eaf-buffer buffer)
-	      (setq eaf-buffer-window (get-buffer-window exists-eaf-buffer))
-	      (throw 'found-match-buffer t)))))
-      (if (and exists-eaf-buffer eaf-buffer-window)
-	  (pop-to-buffer exists-eaf-buffer)
-	(eaf-open url app-name "--synctex_on=True"))))
-
-  (eval-after-load "tex"
-    '(progn
-       (add-to-list 'TeX-view-program-list '("eaf" TeX-eaf-sync-view))
-       (add-to-list 'TeX-view-program-selection '(output-pdf "eaf"))
-       ))
-  )
 
 ;; (setq TeX-view-program-selection
 ;;       '(((output-dvi has-no-display-manager)

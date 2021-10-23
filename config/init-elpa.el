6bb4640c748200adea05535b39d15803b51f2721
yongfei-mint <yongfeizhai@outlook.com>
2021-10-23 23:49:28 +0800

    移除 melpa-stable

--- config/init-elpa.el
+++ config/init-elpa.el
@@ -10,7 +10,7 @@
 ;;; USTC mirrors
 (setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
-                         ("melpa-stable" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/")
+                         ;; ("melpa-stable" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/")
                          ("org" . "http://mirrors.ustc.edu.cn/elpa/org/")))
 
 ;; (package-initialize)

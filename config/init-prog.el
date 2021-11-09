cc6a36eab65488ea37622be035aee2e252177db9
fei-win10-lab <yongfeizhai@outlook.com>
2021-11-09 14:21:47 +0800

    LaTeX: 参考 karthink 的配置，可以正常使用
    直接简单粗暴的把他的 snippets 全部复制过来了，哈哈哈
    
    可能会有点冗余，但是能工作，下次再精细调整吧。

--- config/init-prog.el
+++ config/init-prog.el
@@ -26,10 +26,27 @@
 
 ;;; LaTeX {{
 (add-hook 'LaTeX-mode-hook #'cdlatex-mode)
+(add-hook 'LaTeX-mode-hook #'yas-minor-mode)
+(add-hook 'LaTeX-mode-hook #'laas-mode)
 (with-eval-after-load 'latex
   ;; @ref https://github.com/yangsheng6810/dotfiles/blob/master/.spacemacs.d/init.org
   (add-to-list 'TeX-command-list '
 	       ("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t)))
+
+;;; @REF: https://github.com/karthink/.emacs.d/blob/0d56c66c2e2d53ba05366493f433e523cc36cd87/init.el#L2943
+(defun my/yas-try-expanding-auto-snippets ()
+    (when (and (boundp 'yas-minor-mode) yas-minor-mode)
+      (let ((yas-buffer-local-condition ''(require-snippet-condition . auto)))
+        (yas-expand))))
+(add-hook 'post-self-insert-hook #'my/yas-try-expanding-auto-snippets)
+(with-eval-after-load 'yasnippet
+  (yas-load-directory "~/.emacs.d/snippets/karthink"))
+
+(use-package warnings
+  :config
+  (push '(yasnippet backquote-change) warning-suppress-types)
+  )
+
 ;;; LaTeX }}
 
 ;; devdocs {{

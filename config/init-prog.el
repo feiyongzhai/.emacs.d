053cbf593c5fefb44c0bfb31a4dbc3b4ed96024d
yongfei-mint <yongfeizhai@outlook.com>
2021-11-27 13:16:37 +0800

    paredit ==> smartparens-mode

--- config/init-prog.el
+++ config/init-prog.el
@@ -66,7 +66,14 @@
 ;; devdocs }}
 
 ;;; lisp program related {{
-(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+;; (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode)
+(setq sp-highlight-pair-overlay nil
+      sp-highlight-wrap-overlay nil)
+(define-key smartparens-mode-map (kbd "<C-right>") 'sp-forward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "<C-left>") 'sp-forward-barf-sexp)
+(define-key smartparens-mode-map (kbd "M-U") 'sp-splice-sexp)
 (with-eval-after-load 'paredit
   (define-key paredit-mode-map (kbd "M-s") nil)
   (define-key paredit-mode-map (kbd "M-r") nil)

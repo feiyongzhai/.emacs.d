041e30ee573a0d9244b69afd141d22383c7cb009
yongfei-mint <yongfeizhai@outlook.com>
2021-12-12 21:51:11 +0800

    New command: isearch wrapper `fei-search-forward/backward`
    解决 isearch 和 rime 输入法一些恼人的特性：当开启 rime 的时候 isearch
    会 "吞字" 的情况，而且会一闪一闪，很难受

--- config/init-search.el
+++ config/init-search.el
@@ -10,7 +10,15 @@
 (with-eval-after-load 'isearch
   (define-key isearch-mode-map (kbd "M-g") 'isearch-cancel)
   (define-key isearch-mode-map (kbd "DEL") 'isearch-del-char)
+  ;; M-j 曲线救国的方案，因为通过这样的快捷键设置后，我可以通过连续按
+  ;; 两次 M-j 来完成 force enable 输入法的功能，如果能力够的化，我希望
+  ;; 可以直接一个 M-j 就可以进入 isearch-edit-string 的 minibuffer 中，
+  ;; 并启用输入法
+  (define-key isearch-mode-map (kbd "M-j") 'isearch-edit-string)
   )
+
+(global-set-key (kbd "C-s") 'fei-search-forward)
+(global-set-key (kbd "C-r") 'fei-search-backward)
 
 (global-set-key (kbd "s-s") 'fei-google-search)
 (global-set-key (kbd "M-s p") 'pinyin-search)

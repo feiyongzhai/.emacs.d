b4253dd0bf6f2c0ea6893c1ff1970d427d01f382
yongfei-mint <yongfeizhai@outlook.com>
2022-01-16 20:05:24 +0800

    从零开始使用 ibuffer

--- config/init-ibuffer.el
+++ config/init-ibuffer.el
@@ -2,38 +2,11 @@
 
 ;;; Keys
 
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 (with-eval-after-load 'ibuffer
   (define-key ibuffer-mode-map (kbd "j") 'ibuffer-forward-line)
   (define-key ibuffer-mode-map (kbd "k") 'ibuffer-backward-line)
   (define-key ibuffer-mode-map (kbd "M-j") nil)
   )
 
-;;; Vars
-
-(setq ibuffer-show-empty-filter-groups nil) ;; 不显示空组
-(setq ibuffer-movement-cycle nil)
-(add-hook 'ibuffer-mode-hook #'hl-line-mode)
-;;; 2021-07-02: 小毛病终于解决了，见第6行的配置
-;; (add-hook 'ibuffer-mode-hook #'ibuffer-auto-mode) 
-
-;;; ibuffer group
-(setq ibuffer-saved-filter-groups
-      '(("default"
-	 ("C/C++" (or (name . "^.*\\.c$")
-		      (name . "^.*\\.cpp$")))
-	 ("Matlab" (name . "^.*\\.m$"))
-	 ("Lisp" (or (name . "^.*\\.el$")
-		     (mode . emacs-lisp-mode)))
-	 ("EAF" (mode . eaf-mode))
-	 ("Org" (or (mode . org-mode)
-		    (mode . org-agenda-mode)))
-	 ("Dired" (mode . dired-mode))
-	 ("Emacs" (or (mode . eshell-mode)
-		      (name . "^\\*ielm\\*$")))
-	 ("PDF" (name . "^.*\\.pdf$"))
-	 ;; 下面这个是贪婪匹配，就是匹配 * 开头和结尾所有句子（包括空格）
-	 ("Files" (not (name . "^\\*.*\\*$")))
-	 )))
-
 (provide 'init-ibuffer)
-;;; init-ibuffer.el ends here.

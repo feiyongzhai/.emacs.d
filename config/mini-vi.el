6302e89393d431e086251745faf131a172616441
yongfei-mint <yongfeizhai@outlook.com>
2022-01-25 19:31:41 +0800

    mini-vi.el: add some keybindings

--- config/mini-vi.el
+++ config/mini-vi.el
@@ -4,3 +4,10 @@
 (require 'viper)
 (ido-mode t)
 (setq ido-everywhere t)
+
+(global-set-key (kbd "<mouse-9>") 'neotree-toggle)
+(global-set-key (kbd "<mouse-8>") 'fei-occur-at-point)
+(global-set-key (kbd "C-M-s-~") 'speedbar)
+(with-eval-after-load 'speedbar
+  (define-key speedbar-mode-map (kbd "<mouse-8>") (kbd "b"))
+  (define-key speedbar-mode-map (kbd "<mouse-9>") (kbd "f")))

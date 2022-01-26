6aac63865f596858dd5dfd8fe1bb6c9ae2b7953c
yongfei-mint <yongfeizhai@outlook.com>
2022-01-26 13:47:25 +0800

    Make *ansi-term* can use the same key(M-num) to switch workspace

--- config/init-window-buffer-tab.el
+++ config/init-window-buffer-tab.el
@@ -61,6 +61,21 @@
     ("C-S-t" . tab-new)
     ))
 
+(add-hook 'term-mode-hook
+            (lambda ()
+	      ;; 占用了终端 native 的 M-num 的按键：不过这个按键也不常用，所以没什么问题
+	      (define-key term-raw-map (kbd "M-1") 'eyebrowse-switch-to-window-config-1)
+	      (define-key term-raw-map (kbd "M-2") 'eyebrowse-switch-to-window-config-2)
+	      (define-key term-raw-map (kbd "M-3") 'eyebrowse-switch-to-window-config-3)
+	      (define-key term-raw-map (kbd "M-4") 'eyebrowse-switch-to-window-config-4)
+	      (define-key term-raw-map (kbd "M-5") 'eyebrowse-switch-to-window-config-5)
+	      (define-key term-raw-map (kbd "M-6") 'eyebrowse-switch-to-window-config-6)
+	      (define-key term-raw-map (kbd "M-7") 'eyebrowse-switch-to-window-config-7)
+	      (define-key term-raw-map (kbd "M-8") 'eyebrowse-switch-to-window-config-8)
+	      (define-key term-raw-map (kbd "M-9") 'eyebrowse-switch-to-window-config-9)
+	      (define-key term-raw-map (kbd "M-0") 'eyebrowse-switch-to-window-config-0)
+	      ))
+
 (setq tab-bar-show 1)
 (setq tab-bar-tab-hints t)		;show number in tab-bar
 (setq tab-bar-new-tab-choice nil)	;duplicate

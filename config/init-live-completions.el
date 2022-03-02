4bf9628fb8bff2728c7855b153de515485b9ae09
yongfei-mint <yongfeizhai@outlook.com>
2022-03-02 11:03:47 +0800

    松鼠 `live-completions`

---
+++ config/init-live-completions.el
@@ -0,0 +1,7 @@
+;; live-completions
+(add-to-list 'load-path "~/Repos/live-completions")
+(require 'live-completions)
+(setq temp-buffer-max-height 10)
+(temp-buffer-resize-mode)
+
+(provide 'live-completions)

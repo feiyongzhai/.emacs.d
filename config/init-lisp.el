ecc29ccd068b76f22273e30ec167951c4517f141
yongfei-manjaro <yongfeizhai@qq.com>
2022-05-02 01:16:39 +0800

    new pack rainbow-delimiters-mode

--- config/init-lisp.el
+++ config/init-lisp.el
@@ -1,6 +1,7 @@
 ;; smartparens-mode
 (require 'smartparens-config)
 (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 ;; (add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode)
 (setq sp-highlight-pair-overlay nil
       sp-highlight-wrap-overlay nil)

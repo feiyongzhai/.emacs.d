3f7974a2f1cf3a8a5dda59d3de57a894381cfc53
yongfei-manjaro <yongfeizhai@qq.com>
2022-05-13 00:15:26 +0800

    移除重复配置 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)

--- config/init-prog.el
+++ config/init-prog.el
@@ -1,6 +1,5 @@
 (setq ahk-indentation 4)
 
-(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'prog-mode-hook '+fei-prog-hook)
 (defun +fei-prog-hook ()
   (display-line-numbers-mode t)

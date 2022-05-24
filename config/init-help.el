0806bc717ac8207c050958a93d6d1843ce02fd63
yongfei-manjaro <yongfeizhai@qq.com>
2022-05-24 15:45:52 +0800

    remove rarely usedrarely used command `info-elisp-manual`

--- config/init-help.el
+++ config/init-help.el
@@ -3,16 +3,10 @@
 (global-set-key (kbd "C-h M-c") 'describe-char)
 (global-set-key (kbd "C-h M-f") 'describe-face)
 (global-set-key (kbd "C-h M-k") 'describe-keymap)
-(global-set-key (kbd "C-h E") 'info-elisp-manual) ;C-h R has the same keybinging
 (define-key 'help-command (kbd "C-l") 'find-library)
 (define-key 'help-command (kbd "C-k") 'find-function-on-key)
 (define-key 'help-command (kbd "C-v") 'find-variable)
 (define-key 'help-command (kbd "C-f") 'find-function)
 (define-key 'help-command (kbd "C-i") 'info-display-manual)
-
-(defun info-elisp-manual ()
-  "Display the Emacs manual in Info mode."
-  (interactive)
-  (info "elisp"))
 
 (provide 'init-help)

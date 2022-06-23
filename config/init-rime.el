459deb577480648771ea6dc21fdf37145898ad2a
yongfei-manjaro <yongfeizhai@qq.com>
2022-06-23 11:49:48 +0800

    上一个提交忘记修改函数名了 fei-rime-force-enable -> fei-ime-force-enable

--- config/init-rime.el
+++ config/init-rime.el
@@ -6,7 +6,7 @@
 (register-input-method "rime" "euc-cn" 'rime-activate rime-title)
 
 ;; Keys
-(global-set-key (kbd "M-j") 'fei-rime-force-enable)
+(global-set-key (kbd "M-j") 'fei-ime-force-enable)
 (global-set-key (kbd "M-J") (li (deactivate-input-method)))
 (with-eval-after-load 'rime
   (global-set-key (kbd "C-`") 'rime-select-schema)

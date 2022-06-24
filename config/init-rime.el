442f3acb8821f6661444672a8f1e7859b8ff7887
yongfei-manjaro <yongfeizhai@qq.com>
2022-06-24 16:38:54 +0800

    调整 rime 的快捷键

--- config/init-rime.el
+++ config/init-rime.el
@@ -75,18 +75,18 @@
       (progn
 	(rime-lib-select-schema "luna_pinyin")
 	(setq rime-cycle-zhengma-luna "luna_pinyin")
-	(message "切换到拼音")
+	(message "拼音")
 	)
     (rime-lib-select-schema "zmbig")
     (setq rime-cycle-zhengma-luna "zmbig")
-    (message "切换到郑码巨集")
+    (message "郑码巨集")
     ))
 
 ;; 为了防止快捷键冲突带来的问题，用了这个不太好的快捷键，有优化的空间，
 ;; 但是现在没有优化的必要，但是我发现这个快捷键有一个奇怪的表现：当我
 ;; 按这个快捷键的时候，它确实可以完成切换，但是不会清空已存在的 posframe
 ;; （我的 rime 的候选项是用 posframe 显示的）
-(global-set-key (kbd "s-n") 'rime-cycle-zhengma-luna)
+(define-key rime-mode-map (kbd "C-l") 'rime-cycle-zhengma-luna)
 
 
 (defvar isearch-end-activate-input-method-predicate nil)

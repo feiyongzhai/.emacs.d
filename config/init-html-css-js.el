69aca39aa2de2e44c8c1a3a931258102ccd3d3d5
yongfei-deepin <yongfeizhai@qq.com>
2022-12-30 17:53:28 +0800

    .vue 文件默认启用 web-mode

--- config/init-html-css-js.el
+++ config/init-html-css-js.el
@@ -1,5 +1,6 @@
 ;; 使用 web-mode 来编辑 html，比默认的 html-mode 直观好用一点
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 (add-hook 'web-mode-hook 'emmet-mode)
 
 ;; 用 js2-mode 来代替 js-mode

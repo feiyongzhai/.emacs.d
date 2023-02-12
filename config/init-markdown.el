d50f854501af151aec63953c1521ffe37d4abb44
yongfei-manjaro <yongfeizhai@qq.com>
2023-02-12 14:37:03 +0800

    fix: 避免 open-current-file-with-obsidian 在 windows 平台中的盘符小写问题
    obsidian 的 uri 链接只认大写的盘符（windows平台），同时忽略 obsidian
    emacs 包安装的 elgrep 包产生的文件

--- config/init-markdown.el
+++ config/init-markdown.el
@@ -24,6 +24,13 @@
 (defun open-current-file-with-obsidian ()
   "这个函数只可以打开 obsidian 仓库中的文件"
   (interactive)
-  (browse-url (concat "obsidian://open?path=" (url-hexify-string (buffer-file-name)))))
+  ;; `buffer-file-name' 函数在 windows 平台下，会把盘符转换成小写字母，而 obsidian:// uri
+  ;; 中对于大小写是敏感的，即只认为大写盘符是正确的，如果非要使用 open?path action 的话，
+  ;; 需要在 windows 平台将 `buffer-file-name' 返回值的盘符大写。
+  ;; (browse-url (concat "obsidian://open?path=" (url-hexify-string (buffer-file-name))))
+
+  ;; 下面这个方式避免了 Emacs 中文件名中盘符小写的问题
+  (browse-url (concat "obsidian://open?vault=obsidian&file=" (url-hexify-string (buffer-name))))
+  )
 
 (provide 'init-markdown)

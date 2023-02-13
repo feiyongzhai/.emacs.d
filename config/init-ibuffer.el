db8ff5dcab400576eb84f34c2b908546e0300dbd
yongfei-win10 <yongfeizhai@outlook.com>
2023-02-13 21:21:36 +0800

    Ibuffer: ibuffer-switch-formats 时两种 formats 都显示 all-the-icon

--- config/init-ibuffer.el
+++ config/init-ibuffer.el
@@ -2,6 +2,23 @@
 (add-hook 'ibuffer-mode-hook 'all-the-icons-ibuffer-mode)
 (add-hook 'ibuffer-mode-hook 'hl-line-mode)
 
+(setq all-the-icons-ibuffer-formats
+      '((mark modified read-only locked " "
+	      (icon 2 2 :left :elide)
+	      #(" " 0 1
+		(display
+		 (space :align-to 8)))
+	      (name 18 18 :left :elide)
+	      " "
+	      (size-h 9 -1 :right)
+	      " "
+	      (mode+ 16 16 :left :elide)
+	      " " filename-and-process+)
+	(mark " "
+	      (icon 2 2 :left :elide)
+	      (name 16 -1)
+	      " " filename)))
+
 (with-eval-after-load 'ibuffer
   (define-key ibuffer-mode-map (kbd "j") 'ibuffer-forward-line) ;was `ibuffer-jump-to-buffer'
   (define-key ibuffer-mode-map (kbd "k") 'ibuffer-backward-line) ;was `ibuffer-kill-line'

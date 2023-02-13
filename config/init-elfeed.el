eb117f86ece20140deccc94c6351ee7acab2f485
yongfei-manjaro <yongfeizhai@qq.com>
2023-02-13 17:16:56 +0800

    Elfeed: 重新开始使用 rss 及相关配置

--- config/init-elfeed.el
+++ config/init-elfeed.el
@@ -1,19 +1,21 @@
 (setq elfeed-feeds
-      '(("http://www.solidot.org/index.rss" Solidot)
-	("https://manateelazycat.github.io/feed.xml" ManateeLazyCat)
-	("https://emacstalk.github.io/podcast/index.xml" EmacsTalk)
+      '(("http://www.solidot.org/index.rss" 奇客)
+	("https://manateelazycat.github.io/feed.xml" 懒猫)
+	("https://emacstalk.github.io/podcast/index.xml" emacstalk)
         ;; ("https://karthinks.com/index.xml" Karthinks)
         ;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UC0uTPqBCFIpZxlz_Lv1tk_g" personal video)
 	;; ("https://sachachua.com/blog/feed/" SachaChua)
-        ;; ("https://planet.emacslife.com/atom.xml" emacs community)
+        ("https://planet.emacslife.com/atom.xml" emacsplanet)
 	;; ("https://emacs-china.org/latest.rss" emacs)
         ;; ("http://feed.pippa.io/public/shows/teamhuman" podcast culture)
 	;; ("http://www.geekpark.net/rss" 极客公园)
-	;; ("https://www.zhihu.com/rss" 知乎)
+	("https://www.zhihu.com/rss" 知乎)
 	;; ("https://sspai.com/feed" sspai)
 	))
 
 (with-eval-after-load 'elfeed
+  (face-spec-set 'elfeed-search-title-face '((t (:font "Sarasa Mono SC-12"))))
+
   (define-key elfeed-search-mode-map (kbd "f") 'elfeed-search-show-entry))
 
 ;; (setq shr-inhibit-images t) ;nil

0f8fe780d9e9bad9045f4f9d79321675d10ee923
lab-win10 <yongfeizhai@qq.com>
2023-02-18 13:53:45 +0800

    JSON: 备忘 json 缩进配置

--- config/init-html-css-js.el
+++ config/init-html-css-js.el
@@ -3,9 +3,11 @@
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 (add-hook 'web-mode-hook 'emmet-mode)
 
+;;; 缩进相关问题
 ;; @REF: https://stackoverflow.com/questions/36701024/how-can-i-indent-inline-javascript-in-web-mode
 ;; web-mode 下 <script> tag 的缩进问题
 ;; (setq web-mode-script-padding 4)
+;; (setq js-indent-level 2)
 
 ;; 用 js2-mode 来代替 js-mode
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))

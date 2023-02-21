c40e8a5dcdd5da15ace4be75b7bc3ba3597b9f38
lab-win10 <yongfeizhai@qq.com>
2023-02-21 16:49:37 +0800

    快捷键：<f9> 绑定 recompile

--- config/init-fei.el
+++ config/init-fei.el
@@ -13,7 +13,8 @@
 ;; Misc
 (global-set-key (kbd "C-M-=") 'calculator)
 (global-set-key (kbd "<f7>") 'fei-ff-find-other-file-pdf-org)
-(global-set-key (kbd "<f5>") 'recompile)
+(global-set-key (kbd "<f5>") 'recompile)  ;<f5> 笔记本电脑更好按
+(global-set-key (kbd "<f9>") 'recompile)  ;<f9> 外置的机械键盘更好按
 (global-set-key (kbd "<f12>") 'fei-emacs) ;use for quick debug
 
 (global-set-key (kbd "<insert>") 'fei-occur-at-point)

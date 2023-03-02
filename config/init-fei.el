eaec09763bb2ab33e014f021870647be17588b22
yongfei-manjaro <yongfeizhai@qq.com>
2023-03-02 16:57:17 +0800

    删除文件：把 fei.el 的内容搞到 init-fei.el 中，删除 fei.el

--- config/init-fei.el
+++ config/init-fei.el
@@ -67,5 +67,22 @@
 	   (find-file (concat current-name ".org")))
 	  (t (message "当前不支持这个文件")))))
 
+
+(global-set-key (kbd "C-x M-b") 'switch-to-same-major-mode-buffer)
+
+(defun switch-to-same-major-mode-buffer ()
+  "切换到和当前 buffer 相同 major-mode 的 buffer"
+  (interactive)
+  (let (filtered-buf
+	(cur-major-mode major-mode))
+    (dolist (buf (cdr (buffer-list)))
+      ;; 不显示当前 buffer
+      (with-current-buffer buf
+	(when (eq major-mode cur-major-mode)
+	  (push buf filtered-buf))))
+    (switch-to-buffer
+     (completing-read
+      "Switch to buffer with same major-mode: "
+      (mapcar (lambda (buf) (buffer-name buf)) filtered-buf)))))
 
 (provide 'init-fei)

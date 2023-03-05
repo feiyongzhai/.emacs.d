d75ce901d0947a6bb944eb6bb888900fbe8cb9f9
yongfei-manjaro <yongfeizhai@qq.com>
2023-03-05 22:55:10 +0800

    Org-timer: 给 fei-promodo 添加继续功能 `fei-promodo-continue`

--- extensions/fei/fei-promodo.el
+++ extensions/fei/fei-promodo.el
@@ -1,4 +1,5 @@
 (defvar fei-timer-for-promodo nil)
+(defvar fei-continue-times 0)
 
 (defun fei-promodo-set-timer (time)
   (setq fei-timer-for-promodo
@@ -20,21 +21,32 @@
     ;; 插入一些内容
     (erase-buffer)
     (insert (format "%s" (propertize "时间到了！" 'display '(height 10))))
+    (when (> fei-continue-times 0)
+      (message "已经连续工作 %d 次" fei-continue-times))
 
     ;; 设置快捷键 
     (let ((map (make-sparse-keymap)))
       (set-keymap-parent map (current-local-map))
       (define-key map "q" #'fei-promodo-quit-buffer)
+      (define-key map "c" #'fei-promodo-continue)
       (use-local-map map))
 
     (fit-frame-to-buffer)
 
     ))
 
+(defun fei-promodo-continue ()
+  (interactive)
+  (org-timer-set-timer "20")
+  (jump-to-register ?p)
+  (setq fei-continue-times (1+ fei-continue-times))
+  (message "Fei Promodo 继续... %d 次" fei-continue-times))
+
 (defun fei-promodo-quit-buffer ()
   (interactive)
   ;; fei-timer-for-promodo 是一个不重复的 timer，运行完之后就没结束了，所以不需要 cancel-timer
   ;; (cancel-timer 'fei-timer-for-promodo)
+  (setq fei-continue-times 0)
   (jump-to-register ?p))
 
 (provide 'fei-promodo)

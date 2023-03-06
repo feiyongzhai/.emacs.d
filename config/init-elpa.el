e636060c717bc92e8c1c699da2a7c06b1ca692cb
lab-win10 <yongfeizhai@qq.com>
2023-03-06 21:12:04 +0800

    新包：org-contrib

--- config/init-elpa.el
+++ config/init-elpa.el
@@ -39,7 +39,7 @@
 (setq fei-package-ensure-installed
       '(
 	;; ==== Misc ====
-	minions gcmh tmr
+	minions gcmh tmr org-contrib
 	all-the-icons-ibuffer
 	scratch imenu-list rg
 	rainbow-mode rainbow-delimiters

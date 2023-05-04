87881337e60d2a5d5bf39c44ab745f3efce3c9ec
lab-win10 <yongfeizhai@qq.com>
2023-05-04 16:54:28 +0800

    调整 fori 和 forj snippets 位置 & more
    - `fei-g++-compile-and-run` 适配 windows

--- config/init-c.el
+++ config/init-c.el
@@ -6,9 +6,13 @@
 (defun fei-g++-compile-and-run ()
   (interactive)
   (compile
-   (format "%s '%s' && ./a.out"
-           (if (eq major-mode 'c-mode) "gcc" "g++")
-           (buffer-file-name))
+   (format
+    ;; 不知道在 windows 平台为什么必须是双引号，单引号不行；但在 powershell 或 eshell 中单双引号都可以。
+    ;; 目前猜测是 compile 中对于单双引号在不同系统的解析方式不同。
+    (cond (*is-linux* "%s '%s' && ./a.out")
+          (*is-windows* "%s \"%s\" && a.exe"))
+    (if (eq major-mode 'c-mode) "gcc" "g++")
+    (buffer-file-name))
    t)
   (pop-to-buffer "*compilation*")
   (with-current-buffer "*compilation*"

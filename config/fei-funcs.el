6bbde0219196492114b435a491c5cb8f67f4adf9
yongfei-manjaro <yongfeizhai@qq.com>
2024-01-13 14:39:09 +0800

    把 init-fei.el 的内容移动到 fei-funcs.el 中，删除 init-fei.el

--- config/fei-funcs.el
+++ config/fei-funcs.el
@@ -1,5 +1,17 @@
 ;;; fei-funcs.el === 一些单独工作的小函数
 
+
+;; ========= 自己写的插件 ========
+
+(add-to-list 'load-path "~/.emacs.d/extensions/fei")
+
+(require 'fei-clock)			;fei-clock
+(require 'searchbox)			;searchbox
+(require 'vxref)
+;; (require 'tab-bar-mode-indicator)
+
+
+
 ;; Alias
 (defalias 'a 'org-agenda-list)
 (defalias 'l 'counsel-load-theme)
@@ -464,6 +476,78 @@ INITIAL-DIRECTORY, if non-nil, is used as the root directory for search."
   (set-frame-width nil (+ (if (bound-and-true-p display-line-numbers-mode) 5 0)
 			  (longest-line-colum))))
 
+
+;; require `pulse' library
+(defun fei-pulse-current-line ()
+  (interactive)
+  (require 'hl-line)
+  (pulse-momentary-highlight-one-line (point) 'hl-line))
+
+
+(defun fei-ff-find-other-file-pdf-org ()
+  (interactive)
+  (let* ((name (or (buffer-file-name)
+		   (buffer-name)))
+	 (current-ext (file-name-extension name))
+	 (current-name (file-name-sans-extension name)))
+    (cond ((string= current-ext "org")
+	   (find-file (concat current-name ".pdf")))
+	  ((string= current-ext "md")
+	   (find-file (concat current-name ".pdf")))
+	  ((string= current-ext "pdf")
+	   (find-file (concat current-name ".org")))
+	  (t (message "当前不支持这个文件")))))
+
+
+(defun switch-to-same-major-mode-buffer ()
+  "切换到和当前 buffer 相同 major-mode 的 buffer"
+  (interactive)
+  (let (filtered-buf
+	(cur-major-mode major-mode))
+    (dolist (buf (cdr (buffer-list)))
+      ;; 不显示当前 buffer
+      (with-current-buffer buf
+	(when (eq major-mode cur-major-mode)
+	  (push buf filtered-buf))))
+    (if (length= filtered-buf 0)
+        (message "No same marjo-mode buffer!")
+      (switch-to-buffer
+       (completing-read
+        "Switch to buffer with same major-mode: "
+        (mapcar (lambda (buf) (buffer-name buf)) filtered-buf))))))
+
+(defun switch-to-locked-buffer ()
+  "切换到启用 `emacs-lock-mode' 的 buffer
+
+这样可以把 `emacs-lock-mode' 作为书签来用标记 “重要” buffer 的方法"
+  (interactive)
+  (let (filtered-buf
+	(cur-major-mode major-mode))
+    (dolist (buf (cdr (buffer-list)))
+      ;; 不显示当前 buffer
+      (with-current-buffer buf
+	(when (bound-and-true-p emacs-lock-mode)
+	  (push buf filtered-buf))))
+    (switch-to-buffer
+     (completing-read
+      "Switch to locked buffer: "
+      (mapcar (lambda (buf) (buffer-name buf)) filtered-buf)))))
+
+
+;; 一行流，哈哈
+(defvar pre-frame-height (frame-height))
+
+(defun fei-frame-one-line ()
+  (interactive)
+  (let ((ch (frame-height)))
+    (if (eq ch 5)
+	(set-frame-height nil pre-frame-height)
+      (setq pre-frame-height ch)
+      (set-frame-height nil 5))))
+
+;; (add-hook 'org-capture-mode-hook (lambda () (set-window-text-height nil 1)))
+
+
 ;; 准备弃用的命令
 (defun split-window-right-select ()
   (interactive)
